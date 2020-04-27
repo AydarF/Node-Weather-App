f1696eb7466ad292e84d5b6e70be7358125b7f8a
AydarF <aydar_fayzullin@yahoo.com>
2020-04-27 16:35:23 -0400

    Style news containers

--- public/js/fetchNews.js
+++ public/js/fetchNews.js
@@ -5,14 +5,12 @@ fetch(`/newsData`).then((res) => {
     if (data.error) {
       console.log(data.error);
     } else {
-      console.log(data.newsData.articles);
-
       newsFeed.innerHTML = data.newsData.articles
         .map(function(post) {
           if (post.author === null) {
             post.author = "";
           }
-          return `${`<div class="newsContainer">`}<h4>${post.title}</h4><p>${post.author} ${post.source.name}</p><p>${post.publishedAt}</p><p>${post.description}</p>${`</div>`}`;
+          return `${`<div class="newsContainer">`}<h4>${post.title}</h4><p>${post.author}</p><p>${post.publishedAt}</p><p>${post.description}</p>${`</div>`}`;
         })
         .join("");
     }
