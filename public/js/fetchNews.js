58d89482e1ff9fa23867cb905d491ba23f77d43c
AydarF <aydar_fayzullin@yahoo.com>
2020-05-15 14:00:38 -0400

    Add catch error when fetching news

--- public/js/fetchNews.js
+++ public/js/fetchNews.js
@@ -1,21 +1,27 @@
 let newsFeed = document.querySelector(".newsFeed");
 
-fetch(`/newsData`).then((res) => {
-  res.json().then((data) => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      newsFeed.innerHTML = data.newsData.articles
-        .map(function(post) {
-          if (post.author === null) {
-            post.author = "";
-          }
-          return `${`<div class="newsContainer">`}<h3>${post.title}</h3><p>${post.author}</p><p class="publishedAt">${post.publishedAt}</p><p>${post.description}</p>
+fetch(`/newsData`)
+  .then((res) => {
+    res.json().then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        newsFeed.innerHTML = data.newsData.articles
+          .map(function(post) {
+            if (post.author === null) {
+              post.author = "";
+            }
+            return `${`<div class="newsContainer">`}<h3>${post.title}</h3><p>${post.author}</p><p class="publishedAt">${post.publishedAt}</p><p>${post.description}</p>
          <p><a href=${
            post.url
          } target="_blank" rel="noopener" rel="noreferrer">Read full article [external link]</a></p>${`</div>`}`;
-        })
-        .join("");
-    }
+          })
+          .join("");
+      }
+    });
+  })
+  .catch((error) => {
+    newsFeed.textContent =
+      "The news aren't available at this moment. We're working on fixing the issue. Please come back later";
+    console.log(error.message);
   });
-});
